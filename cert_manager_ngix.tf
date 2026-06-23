003c14c71cff36ae77a637c38e843b8143a09702
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-06-23 14:24:33 +0100

    Add Helm release for cert-manager

---
+++ cert_manager_ngix.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [helm_release.nginx]
+}
