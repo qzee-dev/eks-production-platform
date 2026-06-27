8618decee708bf162e6a1a4030183f46538088a5
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-06-27 22:38:03 +0100

    Comment out S3 backend configuration
    Comment out the Terraform backend configuration in backend_statefile.tf.

--- s3_ecr_iam/backend_statefile.tf
+++ s3_ecr_iam/backend_statefile.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "env/prod/terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-    encrypt = true 
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "my-terraform-state-bucket"
+#     key    = "env/prod/terraform.tfstate"
+#     region = "us-east-1"
+#     use_lockfile = true
+#     encrypt = true 
+#   }
+# }
